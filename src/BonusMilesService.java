4a51b06332dbcaf9bf4c3973679eff3b1d68caa0
anna-mih <anedbalskaya@yandex.ru>
2022-03-21 12:19:51 +0300

    changed main.java, added bonus_miles_class

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate (int ticketPrice) {
+
+        int bonusMiles = ticketPrice / 20;
+
+        return bonusMiles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 200;
-        int bonusRub = 20;
-        int bonusMiles;
+        BonusMilesService service = new BonusMilesService();
+        int ticketPrice = 10_000;
+        int miles = service.calculate(ticketPrice);
+        System.out.println(miles);
 
-        if (ticketPrice >= 0) {
-
-            if (ticketPrice % bonusRub == 0) {
-                bonusMiles = ticketPrice / bonusRub;
-                System.out.println("Вам начислено " + bonusMiles + " бонусных милей.");
-
-            } else {
-                System.out.println("Поставьте стоимость билета кратным " + bonusRub);
-            }
-        } else {
-            System.out.println("Введите стоимость билета больше 0");
-        }
+//        int ticketPrice = 200;
+//        int bonusRub = 20;
+//        int bonusMiles;
+//
+//        if (ticketPrice >= 0) {
+//
+//            if (ticketPrice % bonusRub == 0) {
+//                bonusMiles = ticketPrice / bonusRub;
+//                System.out.println("Вам начислено " + bonusMiles + " бонусных милей.");
+//
+//            } else {
+//                System.out.println("Поставьте стоимость билета кратным " + bonusRub);
+//            }
+//        } else {
+//            System.out.println("Введите стоимость билета больше 0");
+//        }
 
     }
 }
