d1ce0585740b8268d7d204b2f958fa35bf969724
anna-mih <anedbalskaya@yandex.ru>
2022-03-27 11:10:02 +0300

    reformat code in .java files

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,5 @@ public class Main {
         int ticketPrice = 10_000;
         int miles = service.calculate(ticketPrice);
         System.out.println(miles);
-
-//        int ticketPrice = 200;
-//        int bonusRub = 20;
-//        int bonusMiles;
-//
-//        if (ticketPrice >= 0) {
-//
-//            if (ticketPrice % bonusRub == 0) {
-//                bonusMiles = ticketPrice / bonusRub;
-//                System.out.println("Вам начислено " + bonusMiles + " бонусных милей.");
-//
-//            } else {
-//                System.out.println("Поставьте стоимость билета кратным " + bonusRub);
-//            }
-//        } else {
-//            System.out.println("Введите стоимость билета больше 0");
-//        }
-
     }
 }
